1c8fced624f34dbd23ee0c752dcdcd12b25c33fe
Lucas Bonmarin <lucasbonmarin@mbp-de-lucas.home>
2023-04-02 02:03:01 +0200

    Refonte navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,6 +1,10 @@
-import { Bars,Times, Nav, NavBtn, NavBtnLink, NavLink, NavMenu } from "./NavbarElements/NavbarElements";
 import CarIcon from '../../assets/icons/car_logo.svg';
-import { useCallback, useState } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaBars } from 'react-icons/fa';
+import { FaTimes } from 'react-icons/fa';
+
+import './NavBar.scss';
 
 export type NavbarTab = {
     label: string;
@@ -8,6 +12,10 @@ export type NavbarTab = {
 };
 
 const navbarTabs: NavbarTab[] = [
+    {
+        label: "Home",
+        path: "/"
+    },
     {
         label: "About",
         path: "/about"
@@ -34,31 +42,61 @@ const NavBar = () => {
 
     const [clicked, setClicked] = useState<boolean>(false);
 
-    const handleClick = useCallback(() => {
+    const handleClick = () => {
         setClicked(!clicked)
-    }, [clicked]);
+    };
 
     return (
         <>
-            <Nav>
-                <NavLink to="/">
-                    <img src={CarIcon} alt='logo' style={{height: '50px'}} />
-                    <span style={{marginLeft: '10px'}}>
-                        <strong>RENT</strong><br/>a car
-                    </span>
-                </NavLink>
-                { clicked ? <Times onClick={handleClick} /> : <Bars onClick={handleClick} /> }
-                <NavMenu defaultChecked={clicked}>
-                    {navbarTabs.map(tab => (
-                        <NavLink to={tab.path} onClick={() => clicked && handleClick()}>
-                            {tab.label}
-                        </NavLink>
-                    ))}
-                </NavMenu>
-                <NavBtn>
-                    <NavBtnLink to="/sign-in">Sign In</NavBtnLink>
-                </NavBtn>
-            </Nav>
+            <nav>
+                
+                {/* mobile */}
+                <div className={`mobile-navbar ${clicked ? "open-nav" : ""}`}>
+                    <div className="mobile-navbar__close" onClick={handleClick}>
+                        <FaTimes />
+                    </div>
+                    <ul className="mobile-navbar__links">
+                        {navbarTabs.map(tab => (
+                            <li>
+                                <Link onClick={handleClick} to={tab.path}>
+                                    {tab.label}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                {/* desktop */}
+                <div className="navbar">
+                    <div className="navbar__img">
+                        <Link to="/" onClick={() => window.scrollTo(0, 0)}>
+                            <img src={CarIcon} alt='logo' style={{height: '50px'}} />
+                        </Link>
+                    </div>
+                    <ul className="navbar__links">
+                        {navbarTabs.map(tab => (
+                            <li>
+                                <Link to={tab.path}>
+                                    {tab.label}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                    <div className="navbar__buttons">
+                        <Link className="navbar__buttons__sign-in" to="/">
+                            Sign In
+                        </Link>
+                        <Link className="navbar__buttons__register" to="/">
+                            Register
+                        </Link>
+                    </div>
+
+                    { /* mobile */ }
+                    <div className="mobile-hamb" onClick={handleClick}>
+                        <FaBars />
+                    </div>
+                </div>
+            </nav>
         </>
     );
 }
